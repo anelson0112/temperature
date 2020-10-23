91a4601d36459018a5369aeff53015dcef172c68
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-10-23 11:14:48 -0500

    Kelvin added

--- temp.js
+++ temp.js
@@ -15,18 +15,34 @@ function tempConvert(){
     var celsius=
     document.getElementById("celsius").value;
 
+    var kelvin= 
+    document.getElementById("kelvin").value;    
 
-if (fahrenheit !=""){
+if (fahrenheit !=""){ 
+    kelvin=((parseFloat(fahrenheit)-32)*5/9)+273.15;
     celsius = (parseFloat(fahrenheit)-32)/1.8;
-    } else {
+    
+    
+} else if (kelvin !=""){
+    celsius=(parseFloat(kelvin)-273.15);
+    fahrenheit=((parseFloat(kelvin)-273.15)*9/5)+32;
+}
+
+else {
         fahrenheit=(parseFloat(celsius)* 1.8)+32;
-    } 
+        kelvin=(parseFloat(celsius)+273.15);
+    }
+
     
 
  document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
  
  document.getElementById('celsius').value= parseFloat(celsius).toFixed(1);
+ 
+ document.getElementById('kelvin').value = parseFloat(kelvin).toFixed(2);
+ 
 }
+
 
 //define clear function
 function clearForm(){ 
@@ -34,6 +50,8 @@ function clearForm(){
     document.getElementById('fahrenheit').value='';
 
     document.getElementById('celsius').value='';
+
+    document.getElementById('kelvin').value='';
 }
 
 
