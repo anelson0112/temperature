695ccdc76917970b0c52d18a2b02be2a0c1054e4
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-10-26 08:05:04 -0500

    more comments

--- temp.js
+++ temp.js
@@ -8,7 +8,7 @@ document.getElementById('clearForm').onclick=clearForm;
 //define converstion function
 
 function tempConvert(){
-
+//define variables for each temp
     var fahrenheit=
     document.getElementById("fahrenheit").value;
 
@@ -17,24 +17,24 @@ function tempConvert(){
 
     var kelvin= 
     document.getElementById("kelvin").value;    
-
+//first part of function if temp is entered in F
 if (fahrenheit !=""){ 
     kelvin=((parseFloat(fahrenheit)-32)*5/9)+273.15;
     celsius = (parseFloat(fahrenheit)-32)/1.8;
     
-    
+ //second part of function if temp is entered in C
 } else if (kelvin !=""){
     celsius=(parseFloat(kelvin)-273.15);
     fahrenheit=((parseFloat(kelvin)-273.15)*9/5)+32;
 }
-
+//third part of function if temp is entered in K
 else {
         fahrenheit=(parseFloat(celsius)* 1.8)+32;
         kelvin=(parseFloat(celsius)+273.15);
     }
 
     
-
+//call to have number returned with decimals
  document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
  
  document.getElementById('celsius').value= parseFloat(celsius).toFixed(1);
